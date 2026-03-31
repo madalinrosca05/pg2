302a8acf743030941ac6c45870830941de4da3a0
lourencomatias <lourencomatias0606@gmail.com>
2026-03-31 11:42:02 +0100

    prog12 atualizado

--- ex2/prog12.c
+++ ex2/prog12.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
-#include <limits.h>
-#include <stdbool.h>
 #include "calcParity.h"
-/*
-Escreva o programa de teste prog12.c, produzindo o executável prog12, que recebe de standard input
-uma sequência de valores, aplica a função calcParity a cada um deles e apresenta, em standard
-output, os resultados produzidos.
-*/
+
 int main(){
-    
     unsigned int num;
     int paridade;
-    while (true)
+    
+    while (printf("insira a sequência: "), scanf("%u", &num) == 1) // Uso de %u para unsigned
     {
-        printf("insira a sequência: \n");
-        if (scanf("%d", &num)!= 1)
-        {
-            printf("valor inválido, reinicie o programa \n\n");
-            break;
-        }
         paridade = calcParity(num);
-        if (paridade == 1)
-        {
-            printf("%d é Impar!\n", num);
-        }else{
-            printf("%d é Par!\n", num);
+        if (paridade == 1) {
+            printf("%u é Impar!\n", num);
+        } else {
+            printf("%u é Par!\n", num);
         }
     }
     return 0;
 }
-
-        // gcc -Wall -o prog12 prog12.c calcParity.c countOnes.c
